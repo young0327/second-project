39baffd5070d73f86da15434ae2639e8f4ca347f
smhrd <smhrd@DESKTOP-38R0MOL>
2022-01-06 14:50:36 +0900

    게시판 출력 및 카테고리 선택

--- Cloud9/src/main/java/kr/project/web/RestMainController.java
+++ Cloud9/src/main/java/kr/project/web/RestMainController.java
@@ -1,39 +1,46 @@
 package kr.project.web;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mysql.fabric.Response;
+
+import kr.project.domain.Board;
 import kr.project.domain.User;
+import kr.project.service.BoardService;
 import kr.project.service.UserService;
 
 @RestController
 public class RestMainController {
 
 	@Autowired
-	UserService service;
+	UserService userService;
+	@Autowired
+	BoardService boardService;
 	
 	@PostMapping("/joinAjax.do")
 	public void joinAjax(User vo) {
-		service.regist(vo);
+		userService.regist(vo);
 	}
 
 	@GetMapping("/idcheckAjax.do")
 	public int idcheckAjax(String id) {
-		int result = service.idcheckAjax(id);
+		int result =userService.idcheckAjax(id);
 		return result;
 	}
 	
 
 	@PostMapping("/login.do")
 	public String login(User vo, HttpSession session) {
-		User user = service.login(vo);
+		User user = userService.login(vo);
 		String YN;
 		if (user != null) {
 			YN="y";
@@ -45,4 +52,15 @@ public class RestMainController {
 		return YN;
 	}
 
+	@GetMapping("/boardListAjax.do")
+	public String boardListAjax(String cate, HttpSession session){
+		System.out.println("넘어와!");
+		System.out.println("카테고리:"+cate);
+		if(cate.equals("category0")){
+			cate = "category";
+		}
+		List<Board>boardlist= boardService.boardList(cate);
+		session.setAttribute("boardlist",boardlist);
+		return "community";
+	}
 }
--- Cloud9/target/classes/kr/project/domain/user.sql
+++ Cloud9/target/classes/kr/project/domain/user.sql
@@ -10,3 +10,11 @@ insert into userinfo values('test1','1234','test1')
 
 select * from userinfo
 delete from userinfo where nick = ""
+desc proboard
+select * from tsboard
+
+insert into tsboard values('test','test01','테스트입니다.','테스트다아아앙','category0',now())
+insert into tsboard values('hero','test01','테스트입니다.','테스트다아아앙','category0',now())
+insert into tsboard values('new','test01','테스트입니다.','테스트다아아앙','category0',now())
+       select * from tsboard where category like '%category%'
+
